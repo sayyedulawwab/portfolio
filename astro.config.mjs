0d7b1d863a7908276835123033de4bda13c1e5e2
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2024-12-14 22:17:27 +0600

    update seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://sayyedulawwab.com',
+  integrations: [tailwind(), react(), sitemap({})],
   output: 'server',
   adapter: netlify(),
   content: {
