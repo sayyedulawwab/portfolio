719116351a3422e738851779b86b7a77b825bfc2
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2024-12-14 23:49:36 +0600

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,21 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sayyedulawwab.com',
-  integrations: [tailwind(), react(), sitemap({})],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({}),
+    partytown({
+      // Example: Disable debug mode.
+      config: { debug: false },
+      forward: ['dataLayer.push'],
+    }),
+  ],
   output: 'server',
   adapter: netlify(),
   content: {
