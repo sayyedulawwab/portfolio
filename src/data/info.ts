aaeddd58d0dbcfff63d629121ab076c0f858b19e
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2024-12-18 00:13:47 +0600

    update

--- src/data/info.ts
+++ src/data/info.ts
@@ -25,7 +25,7 @@ export const info = {
     {
       title: 'E-commerce Microservices',
       description: `E-commerce Microservices with Catalog, Identity, Ordering, Cart, Payment services using Clean Architecture, Domain Driven Design, CQRS, RabbitMQ message broker for Inter-service communication and Ocelot for API Gateway`,
-      thumbnail: '/assets/images/EcommerceMicroservice.png',
+      thumbnail: '/assets/images/EcommerceMicroservice.webp',
       liveUrl: '',
       githubUrl: 'https://github.com/sayyedulawwab/EcommerceMicroservices',
       built_with: [
@@ -59,7 +59,7 @@ export const info = {
       title: 'Job Posting System of Bdjobs.com',
       description:
         'Contributed to the migration of The Job Posting system of Bdjobs.com Employer panel from legacy system to ASP.NET Core and Angular. built the API using ASP.NET Core following layered architecture in a team setting.',
-      thumbnail: '/assets/images/recruiter-bdjobs-jobposting.png',
+      thumbnail: '/assets/images/recruiter-bdjobs-jobposting.webp',
       liveUrl: 'https://recruiter.bdjobs.com/jobposting/job-information',
       built_with: [
         'Layered Architecture',
@@ -67,103 +67,6 @@ export const info = {
         'ASP.NET Core Web API',
       ],
     },
-
-    // {
-    //   title: 'MERN Shop',
-    //   thumbnail:
-    //     'https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    //   liveUrl: '',
-    //   githubUrl: 'https://github.com/sayyedulawwab/mern-shop',
-    //   built_with: ['React', 'Node', 'Express', 'MongoDB'],
-    // },
-    // {
-    //   title: 'Covid 19 Tracker',
-    //   thumbnail:
-    //     'https://raw.githubusercontent.com/sayyedulawwab/covid-19-tracker/main/covid-19-tracker.png',
-    //     liveUrl: 'https://covid-19-tracker-7b7422.netlify.app/',
-    //     githubUrl: 'https://github.com/sayyedulawwab/covid-19-tracker',
-    //   built_with: [
-    //     'React',
-    //     'disease.sh - Open Disease Data API',
-    //     'Deployed using Netlify',
-    //   ],
-    // },
-    // {
-    //   title: 'Netflix Clone',
-    //   thumbnail:
-    //     'https://raw.githubusercontent.com/sayyedulawwab/netflix-front-clone/main/netflix-front-clone.png',
-    //     liveUrl: 'https://netflix-front-clone.web.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/netflix-front-clone',
-    //   built_with: [
-    //     'React',
-    //     'The Movie Database (TMDB) API',
-    //     'Deployed using Firebase',
-    //   ],
-    // },
-    // {
-    //   title: 'Weatherio',
-    //   thumbnail:
-    //     'https://raw.githubusercontent.com/sayyedulawwab/weatherio/main/weatherio.png',
-    //     liveUrl: 'https://weatherio-76a5a8.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/weatherio',
-    //   built_with: ['React', 'OpenWeather API', 'Deployed using Netlify'],
-    // },
-    // {
-    //   title: 'Checkout page',
-    //   thumbnail:
-    //     'https://raw.githubusercontent.com/sayyedulawwab/checkout-page/main/checkout-page-desktop.png',
-    //     liveUrl: 'https://epic-villani-dd2a49.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/checkout-page',
-    //   built_with: ['HTML', 'CSS', 'JavaScript'],
-    // },
-    // {
-    //   title: 'Recipe page',
-    //   // thumbnail: 'https://raw.githubusercontent.com/sayyedulawwab/recipe-page/main/recipe-blog-desktop.png',
-    //   thumbnail: '/assets/images/recipe-blog-desktop.png',
-    //   liveUrl: 'https://hardcore-morse-56bb64.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/recipe-page',
-    //   built_with: ['HTML', 'CSS'],
-    // },
-    // {
-    //   title: 'My Team page',
-    //   thumbnail:
-    //     'https://raw.githubusercontent.com/sayyedulawwab/my-team-page/main/my-team-page-desktop.png',
-    //   liveUrl: 'https://peaceful-leavitt-0e6de1.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/my-team-page',
-    //   built_with: ['HTML', 'CSS'],
-    // },
-    // {
-    //   title: 'My Gallery',
-    //   thumbnail:
-    //     'https://raw.githubusercontent.com/sayyedulawwab/my-gallery/main/my-gallery-desktop.png',
-    //   liveUrl: 'https://lucid-swanson-8a784c.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/my-gallery',
-    //   built_with: ['HTML', 'CSS'],
-    // },
-    // {
-    //   title: 'Interior Consultant',
-    //   // thumbnail: 'https://raw.githubusercontent.com/sayyedulawwab/interior-consultant/main/interior-consultant-desktop.png',
-    //   thumbnail: '/assets/images/interior-consultant-desktop.png',
-    //   liveUrl: 'https://awesome-jang-98c7ed.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/interior-consultant',
-    //   built_with: ['HTML', 'CSS'],
-    // },
-    // {
-    //   title: 'Edie Homepage',
-    //   // thumbnail: 'https://raw.githubusercontent.com/sayyedulawwab/edie-homepage/main/edie-desktop.png',
-    //   thumbnail: '/assets/images/edie-desktop.png',
-    //   liveUrl: 'https://keen-shaw-b09835.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/edie-homepage',
-    //   built_with: ['HTML', 'CSS'],
-    // },
-    // {
-    //   title: '404 NOT FOUND',
-    //   thumbnail:
-    //     'https://raw.githubusercontent.com/sayyedulawwab/404-not-found/main/404-desktop.png',
-    //   liveUrl: 'https://flamboyant-jepsen-af0878.netlify.app/',
-    //   githubUrl: 'https://github.com/sayyedulawwab/404-not-found',
-    //   built_with: ['HTML', 'CSS'],
-    // },
   ],
 
   experience: [
@@ -270,8 +173,7 @@ export const info = {
     {
       name: 'Mohammad Rafiqul Islam',
       headline: 'Head of Product @Bdjobs',
-      avatar:
-        'https://eu.ui-avatars.com/api/?name=Mohammad+Rafiqul+Islam&size=250',
+      avatar: '/assets/images/recommendation-mri.png',
       description: `"I have had the privilege of working with Muhammad Sayyedul Awwab @bdjobs, and I can confidently say he is an exceptional software engineer with a rare combination of technical expertise and business acumen.
       <br><br>
       Awwab is a professional software engineer who approaches every task with precision and dedication. He has an innate ability to understand product requirements thoroughly, ensuring the technical solutions he develops align seamlessly with business goals. His work consistently reflects a deep understanding of how to fulfill business objectives through the thoughtful application of cutting-edge technologies.
@@ -283,8 +185,7 @@ export const info = {
     {
       name: 'Mizanur Rahman Hridu',
       headline: 'Jr. SDE @ TechnoNext, Ex Bdjobs.com Ltd',
-      avatar:
-        'https://eu.ui-avatars.com/api/?name=Mizanur+Rahman+Hridu&size=250',
+      avatar: '/assets/images/recommendation-mrh.png',
       description: `"I had the pleasure of working closely with Muhammad Sayyedul Awwab and learned a great deal from him. He was instrumental in assisting me during my early days at bdjobs, providing invaluable support and guidance. Together, we collaborated on various projects, including the development of the Job postings system for bdjobs' employer site (recruiter.bdjobs.com) and numerous bug fixes. Additionally, we worked together on a personal Ecommerce project, implementing clean architecture principles. Muhammad is a remarkably positive, honest, and hardworking individual. His optimistic outlook and dedication to his work were truly inspiring. I highly recommend Muhammad for any endeavor he pursues."`,
     },
   ],
