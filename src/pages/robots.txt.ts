a146adae95479686f3d0c1e67e4006bee5d4d285
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2025-03-20 19:41:48 +0600

    update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,11 +3,19 @@ import type { APIRoute } from 'astro';
 const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
-
+Disallow: /~partytown/
 Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
+  if (!site) {
+    return new Response('Site URL not available', { status: 500 });
+  }
+
   const sitemapURL = new URL('sitemap-index.xml', site);
-  return new Response(getRobotsTxt(sitemapURL));
+  return new Response(getRobotsTxt(sitemapURL), {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
 };
